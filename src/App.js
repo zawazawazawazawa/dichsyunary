7ee147c4c9263f9f080a1345cc37f17056c5a5f8
zawazawazawazawa <bosse.de.poulet@gmail.com>
2020-06-28 14:54:27 +0900

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import 'react-calendar/dist/Calendar.css';
 
 import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width                 : '50%',
+    height                : '25%'
+  }
+};
+
+Modal.setAppElement('#root')
 
 function App() {
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState("")
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    console.log("open!!!!")
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+  function handleSubmit(){
+    alert("Submit!!")
+  }
+
   return (
-    <Calendar />
+    <div id="root">
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <form onSubmit={handleSubmit}>
+          <label>
+            Memo:
+            <input type="text" value={inputValue} onChange={ (e) => (setInputValue(e.target.value)) }/>
+          </label>
+        <input type="submit" value="Submit" />
+      </form>
+      </Modal>
+      <Calendar
+        onClickDay={openModal}
+      />
+    </div>
   );
 }
 
